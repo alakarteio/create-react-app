252335ab3fde852432c9d74858d8c4dc07944391
delphinemillet <delphine.millet@zenika.com>
2018-06-08 16:23:03 +0200

    :sparkles: main screen header

--- packages/react-scripts/template/src/app.js
+++ packages/react-scripts/template/src/app.js
@@ -1,26 +1,6 @@
-// import React, { Component } from 'react'
-// import logo from './logo.png'
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App
-
 import React from 'react'
 import Screens from './screens'
+import 'normalize.css'
 
 const App = () => <Screens />
 
--- packages/react-scripts/template/src/screens/main/main.jsx
+++ packages/react-scripts/template/src/screens/main/main.jsx
@@ -4,10 +4,14 @@ import { Link } from '@k-redux-router/react-k-ramel'
 import { component } from 'hoc'
 import styles from './main.styles'
 
-const Main = ({
-  classes,
-}) => (
+import logo from './logo.png'
+
+const Main = ({ classes }) => (
   <div className={classes.main}>
+    <header className={classes.header}>
+      <img src={logo} className={classes.logo} alt="logo" />
+      <h1 className={classes.title}>Welcome to k-ramel</h1>
+    </header>
     <Link code="jokes">>Go to jokes</Link>
   </div>
 )
